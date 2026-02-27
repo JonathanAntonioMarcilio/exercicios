01a43de4fa1d4b3877a770749f8da99e5e5334cc
Jonathan Antonio Marcilio <jonathanmarcilio3011@gmail.com>
2026-02-26 21:10:25 -0300

    exercicio03 completo

--- exercicio03.dart
+++ exercicio03.dart
@@ -1,8 +1,22 @@
-String numero = "42";
+String texto = "42";
+int numero = 100;
 
 
 void main(){
+  print("Número: $texto, Tipo: ${texto.runtimeType}");
+
+  var inteiro = int.parse(texto);
+
+  print("Número: $inteiro, Tipo: ${inteiro.runtimeType}");
+
+  double flutuante = double.parse(texto);
+
+  print("Número: $flutuante, Tipo: ${flutuante.runtimeType}");
+
   print("Número: $numero, Tipo: ${numero.runtimeType}");
 
+  String numTexto = numero.toString();
 
-} 
+  print("Número: $numTexto, Tipo: ${numTexto.runtimeType}");
+
+}
